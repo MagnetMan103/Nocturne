5d7173b3da0ce5ddb490c7d8ee8ff0f6fc80c731
William Liaw <=>
2024-10-05 13:29:17 -0400

    square button

--- components/MoodPage/MoodSelector.tsx
+++ components/MoodPage/MoodSelector.tsx
@@ -1,15 +1,23 @@
-import {View} from "react-native";
+import { View, StyleSheet } from "react-native";
 import MoodButton from "@/components/MoodPage/MoodButton";
 
 export default function MoodSelector() {
-    // make a grid of buttons with different moods that can be selected
-    // use <Text> , <Pressable> components
-    return (
-        <View>
-            <MoodButton mood={"Happy"} />
-            <MoodButton mood={"Sad"} />
-            <MoodButton mood={"Angry"} />
-            <MoodButton mood={"Excited"} />
-        </View>
-    )
+  // make a grid of buttons with different moods that can be selected
+  return (
+    <View style={styles.container}>
+      <MoodButton mood={"Happy"} />
+      <MoodButton mood={"Sad"} />
+      <MoodButton mood={"Angry"} />
+      <MoodButton mood={"Excited"} />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row", // Row direction to start
+    flexWrap: "wrap",     // Wrap items to the next line when they exceed the width
+    justifyContent: "center", // Center align them horizontally
+    alignItems: "center", // Center align them vertically
+  },
+});
